71f0390676c49250c9c5595ddc322e4febfe3b6c
masputrawae <masputrawae@proton.me>
2025-10-17 06:41:23 +0700

    Clean up the code to make it better and easier to fix

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
-a = float(input("First Number: "))
-b = float(input("Last Number: "))
-op = input("Select Operation (+, -, *, /): ")
+def cln_num(x):
+    return int(x) if x.is_integer() else x
 
-if op == "+":
-    print(a + b)
-elif op == "-":
-    print(a - b)
-elif op == "*":
-    print(a * b)
-elif op == "/":
-    if b == 0:
-        print("Cannot be devided by 0")
+def calculator(a, b, op):
+    try:
+        first_num = cln_num(float(a))
+        last_num = cln_num(float(b))
+    except ValueError:
+        return "\n!Input must be a Number"
+
+    result = 0
+
+    if op == "+":
+        result = first_num + last_num
+    elif op == "-":
+        result = first_num - last_num
+    elif op == "*":
+        result = first_num * last_num
+    elif op == "/":
+        if last_num == 0:
+            return "\n!Cannot be divided by 0"
+        else:
+            result = first_num / last_num
     else:
-        print(a / b)
-else:
-    print("Option Not Available")
+        return "\n!The selected operation is not available"
+    
+    return f"\nResult: {first_num} {op} {last_num} = {cln_num(result)}"
+
+def main():
+    print("=== Python Kalkulator ===")
+    a = input("First Number: ")
+    b = input("Last Number: ")
+    op = input("Select Operation (+, -, *, /): ")
+
+    result = calculator(a, b, op)
+    print(result)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print("\n!Error: ", e)
+
+
